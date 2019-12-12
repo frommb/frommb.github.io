b57020b320c25cb0a525e8291918b81199372a07
frommb <fro16004@byui.edu>
2019-12-11 20:13:32 -0700

    Preston 5

--- cit230/lesson5/scripts/main.js
+++ cit230/lesson5/scripts/main.js
@@ -1,74 +1,40 @@
-function toggleMenu() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+//code for responsive menu for small and medium views
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
-var currentDate = new Date();
-var currentDateString;
-
-// get day of week
-var weekDayNumber = currentDate.getDay();
-
-
-var daysOfWeek = [ 
-    'Sunday', 
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
+//code for current date
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
 ];
-
-var weekDay = daysOfWeek[weekDayNumber];
-
-currentDateString = weekDay + ', ';
-
-currentDateString += currentDate.getDate();
-
-var monthsOfYear = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
 ];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+document.getElementById("currentdate").textContent = fulldate;
 
-var monthNumber = currentDate.getMonth();
-
-var month = monthsOfYear[monthNumber];
-
-currentDateString += ' ' + month;
-
-currentDateString += ' ' + currentDate.getFullYear();
-
-document.getElementById('currentDate').innerHTML = currentDateString;
-
-// Create Five day Forcast
-let currentDay = weekDayNumber;
-
-for (let i=1; i<6; i++) {
-    //add one day to current day
-    currendDay++
-
-    // if current day is greater than 6 (Saturday), reset it to 0 for Sunday. 
-    if (currentDay>6) {
-        currentDate=0;
-    }
-
-    // assign value to placeholder in HTML
-    const element = document.getElementById(`day${i}`);
-
-    element.innerHTML = daysOfWeek[currentDay]
-}
-
-//show/hide pancake
-if (weekDayNumber === 5) {
-    document.getElementById("pancake").removeAttribute("class", "hidden");
+//code for banner ad
+if (d.getDay() === 5) {
+    document.getElementById("bannerad").hidden = false;
 }
